7645b8cb99214989158915653f0aca82bd3fa526
Saharalissa <sahar.alissa@gmail.com>
2020-10-05 11:40:12 +0300

    project finalshape

--- main.js
+++ main.js
@@ -2,40 +2,47 @@ var gas=["10w40", "20w50"];
 var dies="15w40";
 var hyb=["0w20", "5w20", "5w30", "0w40"];
 
- // var button1 = document.getElementsByClassName("container");
+var price1 = 5;
+var price2 = 7;
+var price3 = 10;
+
 
 function add() {
 	var petr=document.getElementById("gasoline");
 	var petro=document.getElementById("diesel");
 	var petrol=document.getElementById("hybrid");
-	
-	if(petr.checked===false && petro.checked===false && petrol.checked===false) {
-		alert("YOU MUST SELECT ENGINE TYPE")
-	}else if(petr.checked) {
-     document.getElementById("result").innerHTML="YOU CAN CHOOSE GRADE ''"+gas[0]+"'' OR GRADE ''"+gas[1]+"''"
- } else if(petro.checked) {
- 	document.getElementById("result").innerHTML="YOU SUITABLE GRADE IS ''"+dies+"''"
- } else if(petrol.checked) {
- 	document.getElementById("result").innerHTML="YOU CAN CHOOSE GRADE ''"+hyb[0]+" ''OR GRADE ''"+hyb[1]+"'' OR GRADE ''"+hyb[2]+"'' OR GRADE ''"+hyb[3]+"''"
 
- }
+	
+	if (petr.checked===false && petro.checked===false && petrol.checked===false) {
+		alert("YOU MUST SELECT ENGINE TYPE")
+ 	} else if(petr.checked) {
+     document.getElementById("result").innerHTML="YOU CAN CHOOSE GRADE ''"+gas[0]+"'' OR GRADE ''"+gas[1]+"''"
+ 	} else if(petro.checked) {
+ 	document.getElementById("result").innerHTML="YOU SUITABLE GRADE IS ''"+dies+"''"
+ 	} else if(petrol.checked) {
+ 	document.getElementById("result").innerHTML="YOU CAN CHOOSE GRADE ''"+hyb[0]+" ''OR GRADE ''"+hyb[1]+"'' OR GRADE ''"+hyb[2]+"'' OR GRADE ''"+hyb[3]+"''"
+    }
 }
 
+
 function addSize() {
-var size = document.getElementById("inputA").value
 	
+	var size = document.getElementById("inputA").value	
+
 	if (size === "") {
 		alert("YOU MUST ENTER ENGINE SIZE")
 	} else if (size <= 1200) {
 	document.getElementById("resultAdd").innerHTML = "YOU NEED ("+3+") LITRES OF OIL"
 	} else if(size > 1200 && size <= 2000) {
 	document.getElementById("resultAdd").innerHTML = "YOU NEED ("+4+") LITRES OF OIL"
-}else if(size > 2000 && size <= 3000) {
+	}else if(size > 2000 && size <= 3000) {
 	document.getElementById("resultAdd").innerHTML ="YOU NEED ("+5+") LITRES OF OIL"
-}else if(size > 3000 && size <= 4000) {
+	}else if(size > 3000 && size <= 4000) {
 	document.getElementById("resultAdd").innerHTML = "YOU NEED ("+6+") LITRES OF OIL"
-}else {
+	}else {
 	document.getElementById("resultAdd").innerHTML = "YOU NEED ("+7+") LITRES OF OIL"
+	}
 }
-}
-	
+
+
+
